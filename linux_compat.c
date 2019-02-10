76b983d3bf4278157fc4d9dae2fb4daf9df7918c
Baruch Siach <baruch@tkos.co.il>
2019-02-10 15:34:50 +0200

    linux_compat: implement delay routine
    On MSP430 the compiler provides the __delay_cycles() routine. Implement
    a conservative version for Linux.

---
+++ linux_compat.c
@@ -0,0 +1,20 @@
+/*
+ * Implement Linux compatible routines that tps238x code needs
+ */
+
+#include <time.h>
+
+/* The tps_UpdateSRAMCode() routine needs 100000 cycles to make a 12ms delay.
+ * This means that ~8333 cycles are needed for 1ms. Use a smaller value per ms
+ * (i.e. wait longer) to be on the safe side.
+ */
+#define CYCLES_PER_MS	5000
+#define NSEC_PER_MS	1000000
+
+void __delay_cycles(unsigned int cycles)
+{
+	unsigned int wait_ms = cycles/CYCLES_PER_MS + 1;
+	struct timespec wait_ts = { wait_ms/1000, (wait_ms % 1000) * NSEC_PER_MS };
+
+	nanosleep(&wait_ts, NULL);
+}
---
+++ linux_compat.h
@@ -0,0 +1,10 @@
+#ifndef LINUX_COMPAT_H_
+#define LINUX_COMPAT_H_
+
+/*
+ * Declare Linux compatible routines that tps238x code needs
+ */
+
+void __delay_cycles(unsigned int cycles);
+
+#endif /* LINUX_COMPAT_H_ */
